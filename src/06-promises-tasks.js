27e4646acbe416459ba27a16a68117d61f23cb49
darya-startsava <daryastartsava@gmail.com>
2022-01-21 01:25:27 +0100

    feat: implement promises-tasks functions

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -28,10 +28,23 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  let message = '';
+  if (isPositiveAnswer === true) {
+    message = 'Hooray!!! She said "Yes"!';
+  } else if (isPositiveAnswer === false) {
+    message = 'Oh no, she said "No".';
+  } else {
+    message = 'Wrong parameter is passed! Ask her again.';
+  }
+  return new Promise((resolve, reject) => {
+    if (typeof isPositiveAnswer === 'boolean') {
+      resolve(message);
+    } else {
+      reject(new Error(message));
+    }
+  });
 }
-
 
 /**
  * Return Promise object that should be resolved with array containing plain values.
@@ -48,8 +61,8 @@ function willYouMarryMe(/* isPositiveAnswer */) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array);
 }
 
 /**
@@ -71,8 +84,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array);
 }
 
 /**
